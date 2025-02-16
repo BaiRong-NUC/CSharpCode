4ac5e4033e730e866b75792aac1e8e4e6bf05420
dodamce <3030949391@qq.com>
2025-02-16 19:50:18 +0800

    初始化游戏参数设置

---
+++ EntryPracticeDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntryPracticeDemo
+{
+    class Program
+    {
+        static int _gameWeith = 50;
+        static int _gameHeight = 30;
+        //初始化游戏参数
+        static void InitGame()
+        {
+            //隐藏光标
+            Console.CursorVisible = false;
+            //设置舞台大小
+            Console.SetWindowSize(_gameWeith,_gameHeight);
+            Console.SetBufferSize(_gameWeith, _gameHeight);
+        }
+        static void Main(string[] args)
+        {
+            InitGame();
+        }
+    }
+}
