fd0c19c4604d1c4fb44b9919e2691c4a4cb1ac08
dodamce <3030949391@qq.com>
2025-02-17 18:51:34 +0800

    玩家战斗相关逻辑实现

--- EntryPracticeDemo/Program.cs
+++ EntryPracticeDemo/Program.cs
@@ -22,7 +22,7 @@ namespace EntryPracticeDemo
         public int _playerHp = 100;
         public string _playerIcon = "●";
         public ConsoleColor _playerColor = ConsoleColor.Yellow;
-        public int[] _playerAtkRange = { 1, 10 };
+        public int[] _playerAtkRange = { 1, 13 };
     }
     class Program
     {
@@ -36,6 +36,11 @@ namespace EntryPracticeDemo
 
         static Player _playerInfo = new Player();
 
+        //当前是否战斗
+        static bool _isFight = false;
+
+        //提示信息，当玩家到boss旁提示
+        static string _msg = "";
         //初始化游戏参数
         static void InitGame()
         {
@@ -67,6 +72,28 @@ namespace EntryPracticeDemo
                 Console.Write("■");
             }
         } 
+
+        //判断玩家是否与boss相邻
+        static bool _IsAdjust()
+        {
+            int[] playerPos = _playerInfo._playerPos;
+            int[] bossPos = _bossInfo._bossPos;
+            if ((playerPos[0] == bossPos[0] && Math.Abs(playerPos[1] - bossPos[1]) == 1) || playerPos[1] == bossPos[1] && Math.Abs(playerPos[0] - bossPos[0]) == 2) 
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //擦除信息,从row col位置开始，长度为2*length
+        static void _ClearMsg(int row,int col,int length)
+        {
+            for (int begin = row; begin <= length; begin++)
+            {
+                Console.SetCursorPosition(begin, col);
+                Console.Write(" ");
+            }
+        }
 
         //开始游戏场景
         static void BeginGameMenue()
@@ -143,64 +170,159 @@ namespace EntryPracticeDemo
                 Console.Write(_playerInfo._playerIcon);
                 //检测玩家输入
                 char input = Console.ReadKey(true).KeyChar;
-                //擦除之前的位置
-                Console.SetCursorPosition(_playerInfo._playerPos[0], _playerInfo._playerPos[1]);
-                Console.Write("  ");
-                //改位置
-                switch (input)
+                if (_isFight == false)
                 {
-                    case 'W':
-                    case 'w':
-                        _playerInfo._playerPos[1] -= 1;
-                        if (_playerInfo._playerPos[1] < 1)
-                        {
-                            _playerInfo._playerPos[1] = 1;
-                        }
-                        if (_playerInfo._playerPos[0] == _bossInfo._bossPos[0] && _playerInfo._playerPos[1] == _bossInfo._bossPos[1] && _bossInfo._bossHp > 0) 
-                        {
+                    //非战斗状态
+                    //擦除之前的位置
+                    Console.SetCursorPosition(_playerInfo._playerPos[0], _playerInfo._playerPos[1]);
+                    Console.Write("  ");
+                    //改位置
+                    switch (input)
+                    {
+                        case 'W':
+                        case 'w':
+                            _playerInfo._playerPos[1] -= 1;
+                            if (_playerInfo._playerPos[1] < 1)
+                            {
+                                _playerInfo._playerPos[1] = 1;
+                            }
+                            if (_playerInfo._playerPos[0] == _bossInfo._bossPos[0] && _playerInfo._playerPos[1] == _bossInfo._bossPos[1] && _bossInfo._bossHp > 0)
+                            {
+                                _playerInfo._playerPos[1] += 1;
+                            }
+                            break;
+                        case 'S':
+                        case 's':
                             _playerInfo._playerPos[1] += 1;
-                        }
-                        break;
-                    case 'S':
-                    case 's':
-                        _playerInfo._playerPos[1] += 1;
-                        if (_playerInfo._playerPos[1] >= _gameHeight - 7)
-                        {
-                            _playerInfo._playerPos[1] -= 1;
-                        }
-                        if (_playerInfo._playerPos[0] == _bossInfo._bossPos[0] && _playerInfo._playerPos[1] == _bossInfo._bossPos[1] && _bossInfo._bossHp > 0)
-                        {
-                            _playerInfo._playerPos[1] -= 1;
-                        }
-                        break;
-                    case 'A':
-                    case 'a':
-                        _playerInfo._playerPos[0] -= 2;
-                        if (_playerInfo._playerPos[0] < 2)
-                        {
-                            _playerInfo._playerPos[0] = 2;
-                        }
-                        if (_playerInfo._playerPos[0] == _bossInfo._bossPos[0] && _playerInfo._playerPos[1] == _bossInfo._bossPos[1] && _bossInfo._bossHp > 0)
-                        {
+                            if (_playerInfo._playerPos[1] >= _gameHeight - 7)
+                            {
+                                _playerInfo._playerPos[1] -= 1;
+                            }
+                            if (_playerInfo._playerPos[0] == _bossInfo._bossPos[0] && _playerInfo._playerPos[1] == _bossInfo._bossPos[1] && _bossInfo._bossHp > 0)
+                            {
+                                _playerInfo._playerPos[1] -= 1;
+                            }
+                            break;
+                        case 'A':
+                        case 'a':
+                            _playerInfo._playerPos[0] -= 2;
+                            if (_playerInfo._playerPos[0] < 2)
+                            {
+                                _playerInfo._playerPos[0] = 2;
+                            }
+                            if (_playerInfo._playerPos[0] == _bossInfo._bossPos[0] && _playerInfo._playerPos[1] == _bossInfo._bossPos[1] && _bossInfo._bossHp > 0)
+                            {
+                                _playerInfo._playerPos[0] += 2;
+                            }
+                            break;
+                        case 'D':
+                        case 'd':
                             _playerInfo._playerPos[0] += 2;
-                        }
-                        break;
-                    case 'D':
-                    case 'd':
-                        _playerInfo._playerPos[0] += 2;
-                        if (_playerInfo._playerPos[0]>= _gameWeith - 2)
+                            if (_playerInfo._playerPos[0] >= _gameWeith - 2)
+                            {
+                                _playerInfo._playerPos[0] -= 2;
+                            }
+                            if (_playerInfo._playerPos[0] == _bossInfo._bossPos[0] && _playerInfo._playerPos[1] == _bossInfo._bossPos[1] && _bossInfo._bossHp > 0)
+                            {
+                                _playerInfo._playerPos[0] -= 2;
+                            }
+                            break;
+                        //开始战斗
+                        case 'J':
+                        case 'j':
+                            //变为战斗状态
+                            _isFight = true;
+                            if (_bossInfo._bossHp > 0 && _IsAdjust() == true)
+                            {
+                                Console.SetCursorPosition(2, _gameHeight - 4);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.Write("当前玩家血量{0}", _playerInfo._playerHp);
+                            }
+                            break;
+                    }
+                    if (_bossInfo._bossHp > 0 && _IsAdjust() == true)
+                    {
+                        //相邻时修改提示信息
+                        _msg = "战斗开始，请按J键攻击";
+                    }
+                    else
+                    {
+                        //不相邻清空提示
+                        _ClearMsg(2, _gameHeight - 5, _msg.Length * 2);
+                        _msg = "";
+                    }
+                    //打印提示
+                    Console.SetCursorPosition(2, _gameHeight - 5);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(_msg);
+                }
+                else
+                {
+                    //战斗状态
+                    if (input == 'J' || input == 'j')
+                    {
+                        //判断当前血量是否结束游戏
+                        if (_playerInfo._playerHp <= 0 || _bossInfo._bossHp <= 0)
                         {
-                            _playerInfo._playerPos[0] -= 2;
+                            if (_playerInfo._playerHp <= 0)
+                            {
+                                //跳转游戏结束场景
+                                _sceneId = 3;
+                                break;
+                            }
+                            else
+                            {
+                                //玩家胜利，擦除boss                           
+                                Console.SetCursorPosition(_bossInfo._bossPos[0], _bossInfo._bossPos[1]);
+                                Console.Write("  ");
+                                _isFight = false;
+                                //break;
+                            }
                         }
-                        if (_playerInfo._playerPos[0] == _bossInfo._bossPos[0] && _playerInfo._playerPos[1] == _bossInfo._bossPos[1] && _bossInfo._bossHp > 0)
+                        else
                         {
-                            _playerInfo._playerPos[0] -= 2;
+                            //随机攻击力
+                            Random random = new Random();
+                            int playerAtk = random.Next(_playerInfo._playerAtkRange[0], _playerInfo._playerAtkRange[1]);
+                            //boss血量减少
+                            _bossInfo._bossHp -= playerAtk;
+                            //打印信息
+                            //擦除原有位置的信息,这里20是当前玩家血量{0}的大概长度，硬编码
+                            _ClearMsg(2, _gameHeight - 4, 40);
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.SetCursorPosition(2, _gameHeight - 4);
+                            Console.Write("玩家造成{0}点伤害，boss剩余血量{1}", playerAtk, _bossInfo._bossHp);
+
+                            if (_bossInfo._bossHp > 0)
+                            {
+                                //boss攻击
+                                int bossAtk = random.Next(_bossInfo._bossAtkRange[0], _bossInfo._bossAtkRange[1]);
+                                //玩家血量减少
+                                _playerInfo._playerHp -= bossAtk;
+                                _ClearMsg(2, _gameHeight - 3, 40);
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.SetCursorPosition(2, _gameHeight - 3);
+                                Console.Write("boss造成{0}点伤害，玩家剩余血量{1}", bossAtk, _playerInfo._playerHp);
+                                if( _playerInfo._playerHp <= 0)
+                                {
+                                    //boss胜利
+                                    _ClearMsg(2, _gameHeight - 3, 40);
+                                    Console.SetCursorPosition(2, _gameHeight - 3);
+                                    Console.Write("玩家死亡，游戏结束!");
+                                }
+                            }
+                            else
+                            {
+                                //玩家胜利擦除战斗信息
+                                _ClearMsg(2, _gameHeight - 5, 40);
+                                _ClearMsg(2, _gameHeight - 4, 40);
+                                _ClearMsg(2, _gameHeight - 3, 40);
+                                Console.SetCursorPosition(2, _gameHeight - 4);
+                                Console.Write("玩家胜利，快前往公主前按J继续！");
+                            }
                         }
-                        break;
-                    //开始战斗
-                    case 'J':
-                    case 'j':
-                        break;
+                    }
                 }
             }
         }
